e309b7e876f467cae27dd73f29563d511ccc30ce
Daniel Garcia Lamas <daniel@intelinet.com.br>
2015-04-14 11:29:38 -0300

    Search Form Validation

--- apps/serviceStatusTool/web/controllers/SearchController.js
+++ apps/serviceStatusTool/web/controllers/SearchController.js
@@ -2,13 +2,12 @@ var SearchController = function ($scope, $http){
 
 	var urlBase= contextPath + "/ws";
 	$scope.doSearch = function doSearch() {
-		
+		$scope.error = false;
 		if( ($scope.search == undefined)) {
 			$scope.error = true;
-			$scope.messageError = "Please fill in the required fields OHS Order/Service or Customer/Site Address/Site City";
-		}
-		else  // service and order <> null
-			if(
+			$scope.messageError = "OHS Order/Service or Customer/Site Address/Site City is required";
+		// service and order <> null
+		} else if(
 				((($scope.search.service != "" &&  $scope.search.service != undefined)  && 
 				  ($scope.search.order != "" && $scope.search.order != undefined)) &&
 					(($scope.search.address != undefined && $scope.search.address != "") ||  
@@ -20,7 +19,7 @@ var SearchController = function ($scope, $http){
 				 ) ) {
 				
 				$scope.error = true;
-				$scope.messageError = "Please fill only OHS Order and Service.";
+				$scope.messageError = "Only OHS Order or Service is required ";
 		} else if (	
 					!(
 						($scope.search.service != "" &&  $scope.search.service != undefined)  &&
@@ -52,48 +51,115 @@ var SearchController = function ($scope, $http){
 						 )
 					) 
 					) {
-						var result = "Please fill only ";
+						var result = "Only ";
 						if ($scope.search.order != "" &&  $scope.search.order != undefined ) {
-							result += "OHS Order or Customer and Site Address and Site City.";
+							result += "OHS Order or Customer and Site Address and Site City is required.";
 						} else if ($scope.search.service != "" &&  $scope.search.service != undefined) {
-							result += "Service or Customer and Site Address and Site City.";
+							result += "Service or Customer and Site Address and Site City is required.";
 						}
 						$scope.error = true;
 						$scope.messageError = result;
-		} else if ($scope.search.city != undefined && $scope.search.city != "") {
-			if ( (
-					(
-						($scope.search.customer == "" || $scope.search.customer == undefined) &&  
-						($scope.search.address == "" || $scope.search.address == undefined)
-					 ) ||
-					 (
-						($scope.search.customer != "" || $scope.search.customer != undefined) && 
-						($scope.search.address == "" || $scope.search.address == undefined)
-					 ) ||
-					 (
-						($scope.search.customer == "" || $scope.search.customer == undefined) && 
-						($scope.search.address != "" || $scope.search.address != undefined)
-					 )
-				 )
-			) {	
-				alert("city1");
+		} else if ( ($scope.search.service == "" ||  $scope.search.service == undefined) || ($scope.search.order == "" ||  $scope.search.order == undefined) ) {
+			var msg = "Site1 Address/Site1 City or Site2 Address/Site2 City is required";
+			if($scope.search.customer == undefined || $scope.search.customer == "") {
+				$scope.error = true;
+				$scope.messageError = "Customer is required";
+			//C1
+			} else if( $scope.search.customer != undefined && $scope.search.customer != "" && $scope.search.address != undefined && $scope.search.address != "" && 
+						($scope.search.city == undefined || $scope.search.city == "") && 
+						($scope.search.address2 == undefined || $scope.search.address2 == "") && ($scope.search.city2 == undefined || $scope.search.city2 == "") ) {
+					$scope.error = true;
+					$scope.messageError = "Site1 City  is required";
+			} else if( $scope.search.customer != undefined && $scope.search.customer != "" && $scope.search.address != undefined && $scope.search.address != "" && 
+					($scope.search.city == undefined || $scope.search.city == "") && 
+					($scope.search.address2 != undefined && $scope.search.address2 != "") && ($scope.search.city2 == undefined || $scope.search.city2 == "") ) {
+				$scope.error = true;
+				$scope.messageError = msg;
+			} else if( $scope.search.customer != undefined && $scope.search.customer != "" && $scope.search.address != undefined && $scope.search.address != "" && 
+						($scope.search.city == undefined || $scope.search.city == "") && 
+						($scope.search.address2 == undefined || $scope.search.address2 == "") && ($scope.search.city2 != undefined && $scope.search.city2 != "") ) {
+					$scope.error = true;
+					$scope.messageError = msg;
+			} else if( $scope.search.customer != undefined && $scope.search.customer != "" && $scope.search.address != undefined && $scope.search.address != "" && 
+					($scope.search.city == undefined || $scope.search.city == "") && 
+					($scope.search.address2 != undefined && $scope.search.address2 != "") && ($scope.search.city2 != undefined && $scope.search.city2 != "") ) {
+				$scope.error = true;
+				$scope.messageError = msg;
+			//A1	
+			} else if( $scope.search.customer != undefined && $scope.search.customer != "" && $scope.search.city != undefined && $scope.search.city != "" && 
+					($scope.search.address == undefined || $scope.search.address == "") && 
+					($scope.search.address2 == undefined || $scope.search.address2 == "") && ($scope.search.city2 == undefined || $scope.search.city2 == "") ) {
+				$scope.error = true;
+				$scope.messageError = "Site1 Address is required";
+			} else if( $scope.search.customer != undefined && $scope.search.customer != "" && $scope.search.city != undefined && $scope.search.city != "" && 
+					($scope.search.address == undefined || $scope.search.address == "") && 
+					($scope.search.address2 != undefined && $scope.search.address2 != "") && ($scope.search.city2 == undefined || $scope.search.city2 == "") ) {
+				$scope.error = true;
+				$scope.messageError = msg;
+			} else if( $scope.search.customer != undefined && $scope.search.customer != "" && $scope.search.city != undefined && $scope.search.city != "" && 
+						($scope.search.address == undefined || $scope.search.address == "") && 
+						($scope.search.address2 == undefined || $scope.search.address2 == "") && ($scope.search.city2 != undefined && $scope.search.city2 != "") ) {
+					$scope.error = true;
+					$scope.messageError = msg;
+			} else if( $scope.search.customer != undefined && $scope.search.customer != "" && $scope.search.city != undefined && $scope.search.city != "" && 
+					($scope.search.address == undefined || $scope.search.address == "") && 
+					($scope.search.address2 != undefined && $scope.search.address2 != "") && ($scope.search.city2 != undefined && $scope.search.city2 != "") ) {
+			$scope.error = true;
+			$scope.messageError = msg;
+			//C2
+			} else if( $scope.search.customer != undefined && $scope.search.customer != "" && $scope.search.address2 != undefined && $scope.search.address2 != "" && 
+						($scope.search.city2 == undefined || $scope.search.city2 == "") && 
+						($scope.search.address == undefined || $scope.search.address == "") && ($scope.search.city == undefined || $scope.search.city == "") ) {
+					$scope.error = true;
+					$scope.messageError = "Site2 City  is required";
+			} else if( $scope.search.customer != undefined && $scope.search.customer != "" && $scope.search.address2 != undefined && $scope.search.address2 != "" && 
+					($scope.search.city2 == undefined || $scope.search.city2 == "") && 
+					($scope.search.address != undefined && $scope.search.address != "") && ($scope.search.city == undefined || $scope.search.city == "") ) {
+				$scope.error = true;
+				$scope.messageError = msg;
+			} else if( $scope.search.customer != undefined && $scope.search.customer != "" && $scope.search.address2 != undefined && $scope.search.address2 != "" && 
+						($scope.search.city2 == undefined || $scope.search.city2 == "") && 
+						($scope.search.address == undefined || $scope.search.address == "") && ($scope.search.city != undefined && $scope.search.city != "") ) {
+					$scope.error = true;
+					$scope.messageError = msg;
+			} else if( $scope.search.customer != undefined && $scope.search.customer != "" && $scope.search.address2 != undefined && $scope.search.address2 != "" && 
+					($scope.search.city2 == undefined || $scope.search.city2 == "") && 
+					($scope.search.address != undefined && $scope.search.address != "") && ($scope.search.city != undefined && $scope.search.city != "") ) {
+				$scope.error = true;
+				$scope.messageError = msg;
+			//A2
+			} else if( $scope.search.customer != undefined && $scope.search.customer != "" && $scope.search.city2 != undefined && $scope.search.city2 != "" && 
+					($scope.search.address2 == undefined || $scope.search.address2 == "") && 
+					($scope.search.address == undefined || $scope.search.address == "") && ($scope.search.city == undefined || $scope.search.city == "") ) {
+				$scope.error = true;
+				$scope.messageError = "Site2 Address is required";
+			} else if( $scope.search.customer != undefined && $scope.search.customer != "" && $scope.search.city2 != undefined && $scope.search.city2 != "" && 
+					($scope.search.address2 == undefined || $scope.search.address2 == "") && 
+					($scope.search.address != undefined && $scope.search.address != "") && ($scope.search.city == undefined || $scope.search.city == "") ) {
+				$scope.error = true;
+				$scope.messageError = msg;
+			} else if( $scope.search.customer != undefined && $scope.search.customer != "" && $scope.search.city2 != undefined && $scope.search.city2 != "" && 
+						($scope.search.address2 == undefined || $scope.search.address2 == "") && 
+						($scope.search.address == undefined || $scope.search.address == "") && ($scope.search.city != undefined && $scope.search.city != "") ) {
+					$scope.error = true;
+					$scope.messageError = msg;
+			} else if( $scope.search.customer != undefined && $scope.search.customer != "" && $scope.search.city2 != undefined && $scope.search.city2 != "" && 
+					($scope.search.address2 == undefined || $scope.search.address2 == "") && 
+					($scope.search.address != undefined && $scope.search.address != "") && ($scope.search.city != undefined && $scope.search.city != "") ) {
+			$scope.error = true;
+			$scope.messageError = msg;
+			} else if( $scope.search.address != undefined && $scope.search.address != "" && $scope.search.city != undefined && $scope.search.city != "" && 
+						$scope.search.address2 != undefined && $scope.search.address2 != "" && $scope.search.city2 != undefined && $scope.search.city2 != "" ) {
+					$scope.error = true;
+					$scope.messageError = msg;
+			} else if( ( (($scope.search.address == undefined || $scope.search.address == "") && ($scope.search.city == undefined || $scope.search.city == "")) && 
+						(($scope.search.address2 == undefined || $scope.search.address2 == "") && ($scope.search.city2 == undefined || $scope.search.city2 == "")) ) ) {
+				$scope.error = true;
+				$scope.messageError = msg;
 			}
-			if ((
-					(
-						($scope.search.customer != "" && $scope.search.customer != undefined) &&  
-						($scope.search.address != "" && $scope.search.address != undefined)
-					)
-				 ) && 
-				 (
-					 ($scope.search.city2 != "" && $scope.search.city2 != undefined) || 
-					 ($scope.search.address2 != "" && $scope.search.address2 != undefined) ||
-					 ($scope.search.order != "" && $scope.search.order != undefined) ||
-					 ($scope.search.service != "" && $scope.search.service != undefined)
-				 )
-				) {
-				alert("aaaaaaaaa");
-			}
-		} else {
+		}
+		
+		if(!$scope.error) {
 			var resp = $http({
 			  method  : 'POST',
 			  url     : urlBase + '/getCircuits',
