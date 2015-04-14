18c8b97cece35937199f643980dc2db83a270371
Jackson <jackson@intelinet.com.br>
2015-04-14 08:42:21 -0300

    Log in class SearchService.

--- apps/serviceStatusTool/src/com/colt/controller/SearchService.java
+++ apps/serviceStatusTool/src/com/colt/controller/SearchService.java
@@ -1,18 +1,18 @@
 package com.colt.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.colt.dao.AmnDAO;
-import com.colt.util.Util;
 import com.colt.ws.biz.Circuit;
 import com.colt.ws.biz.Response;
 import com.colt.ws.biz.Search;
@@ -26,40 +26,47 @@ public class SearchService {
 	@PersistenceContext(unitName = "mainDatabase")
 	private EntityManager em;
 
+	private Log log = LogFactory.getLog(SearchService.class);
+
 	@RequestMapping(value = "/getCircuits", method = RequestMethod.POST, headers = "Accept=application/json")
 	public Object getCircuits(@RequestBody Search search) {
+		log.info("Entering method getCircuits()");
 		Response response = null;
 		try {
 			AmnDAO amnDAO = new AmnDAO(em);
 			response = amnDAO.retrieveCircuits(search);
 		} catch (Exception e) {
-			e.printStackTrace();
+			log.error(e);
 			response = new Response();
 			response.setStatus(Response.FAIL);
 			response.setErrorCode(Response.CODE_UNKNOWN);
 			response.setErrorMsg(e.getMessage());
 		}
+		log.info("Exit method getCircuits()");
 		return response;
 	}
 
 	@RequestMapping(value = "/getServiceDetail", method = RequestMethod.POST, headers = "Accept=application/json")
 	public Object getServiceDetail(@RequestBody String id) {
+		log.info("Entering method getServiceDetail()");
 		Response response = null;
 		try {
 			AmnDAO amnDAO = new AmnDAO(em);
 			response = amnDAO.retrieveServiceDetails(id);
 		} catch (Exception e) {
-			e.printStackTrace();
+			log.error(e);
 			response = new Response();
 			response.setStatus(Response.FAIL);
 			response.setErrorCode(Response.CODE_UNKNOWN);
 			response.setErrorMsg(e.getMessage());
 		}
+		log.info("Exit method getServiceDetail()");
 		return response;
 	}
 
 	@RequestMapping(value = "/getTickets", method = RequestMethod.POST, headers = "Accept=application/json")
 	public Object getTickets(@RequestBody Circuit circuit) {
+		log.info("Entering method getTickets()");
 		Response response = new Response();
 		try {
 			SiebelCallRequest sielbelRequest = new SiebelCallRequest();
@@ -77,11 +84,13 @@ public class SearchService {
 				response.setErrorMsg("Not result found.");
 			}
 		} catch (Exception e) {
+			log.error(e);
 			response = new Response();
 			response.setStatus(Response.FAIL);
 			response.setErrorCode(Response.CODE_UNKNOWN);
 			response.setErrorMsg(e.getMessage());
 		}
+		log.info("Exit method getTickets()");
 		return response;
 	}
 }
