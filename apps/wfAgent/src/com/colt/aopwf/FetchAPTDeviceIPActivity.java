7943465223c2b471d2e6d26f94597f4226a11cf6
Daniel Garcia Lamas <daniel@intelinet.com.br>
2015-07-20 10:00:35 -0300

    Bug 3884 - XNG IP not being used in agent

--- apps/wfAgent/src/com/colt/aopwf/FetchAPTDeviceIPActivity.java
+++ apps/wfAgent/src/com/colt/aopwf/FetchAPTDeviceIPActivity.java
@@ -43,6 +43,11 @@ public class FetchAPTDeviceIPActivity implements IWorkflowProcessActivity {
 						}
 					}
 				}
+
+				if ((ipAddress == null || ipAddress.equals("")) && deviceDetails.getType().equals("LANLink")) {
+					ipAddress = deviceDetails.getIp();
+				}
+
 				if(ipAddress != null && !"".equals(ipAddress)) {
 					deviceDetails.setIp(ipAddress);
 					resp = new String[] {"FETCH_DEVICE_DONE"};
